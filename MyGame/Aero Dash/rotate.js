789ee91c0045bd284f089723cce2d80eb66d50e9
SaswatGit <126791441+SaswatGit@users.noreply.github.com>
2023-03-13 21:09:02 +0530

    Delete rotate.js

--- MyGame/Aero Dash/rotate.js
+++
@@ -1,7 +0,0 @@
-screen.orientation.lock('landscape')
-    .then(function() {
-        console.log('Screen orientation locked to landscape mode');
-    })
-    .catch(function(error) {
-        console.error('Error locking screen orientation:', error);
-    });
